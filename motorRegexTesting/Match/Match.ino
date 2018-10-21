b898f3b43f9494571617e359f95fd6f79b026e52
Mike Lundquist <mlundqu2@masonlive.gmu.edu>
2018-10-21 19:13:46 -0400

    mend

--- motorRegexTesting/Match/Match.ino
+++ motorRegexTesting/Match/Match.ino
@@ -1,33 +1,14 @@
 #include <Regexp.h>
 
 static const char DIR_PATTERN [] = "^MSv2_[67]%x_direction_[1-4]_[0-2]$";
+static const char SHIELD_PATTERN_START [] = "^MSv2_[67]%x_";
 
 void setup ()
 {
   Serial.begin (9600);
+  String message = "MSv2_60_direction_2_1";
+  Serial.println("hi");
 
-  // match state object
-  MatchState ms;
-  String message = "MSv2_60_direction_1_1";
-  // what we are searching (the target)
-  char buf [message.length() + 1];
-  message.toCharArray(buf, message.length() + 1);
-  ms.Target (buf);  // set its address
-  ms.Match("random");
-  Serial.println (buf);
-
-  //char result = );
-  
-  if (ms.Match (DIR_PATTERN) > 0)
-    {
-    Serial.print ("Found match at: ");
-    Serial.println (ms.MatchStart);        // 16 in this case     
-    Serial.print ("Match length: ");
-    Serial.println (ms.MatchLength);       // 3 in this case
-    }
-  else
-    Serial.println ("No match.");
-    
-}  // end of setup  
-
+  Serial.println(message.substring(20,21));
+}
 void loop () {}
