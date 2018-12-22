996040a101cd4ed91f3fbb4145803aec01805a30
Michael Lundquist <michaellundquist7@gmail.com>
2018-12-21 22:11:54 -0500

    Adding example code for using multiple stepper motors simultaneously.

--- motorRegexTesting/Match/Match.ino
+++ motorRegexTesting/Match/Match.ino
@@ -1,83 +1,101 @@
-// --------------------------------------
-// i2c_scanner
+// Shows how to run three Steppers at once with varying speeds
 //
-// Version 1
-//    This program (or code that looks like it)
-//    can be found in many places.
-//    For example on the Arduino.cc forum.
-//    The original author is not know.
-// Version 2, Juni 2012, Using Arduino 1.0.1
-//     Adapted to be as simple as possible by Arduino.cc user Krodal
-// Version 3, Feb 26  2013
-//    V3 by louarnold
-// Version 4, March 3, 2013, Using Arduino 1.0.3
-//    by Arduino.cc user Krodal.
-//    Changes by louarnold removed.
-//    Scanning addresses changed from 0...127 to 1...119,
-//    according to the i2c scanner by Nick Gammon
-//    http://www.gammon.com.au/forum/?id=10896
-// Version 5, March 28, 2013
-//    As version 4, but address scans now to 127.
-//    A sensor seems to use address 120.
-// Version 6, November 27, 2015.
-//    Added waiting for the Leonardo serial communication.
-//
-//
-// This sketch tests the standard 7-bit addresses
-// Devices with higher bit address might not be seen properly.
-//
- 
+// Requires the Adafruit_Motorshield v2 library 
+//   https://github.com/adafruit/Adafruit_Motor_Shield_V2_Library
+// And AccelStepper with AFMotor support 
+//   https://github.com/adafruit/AccelStepper
+
+// This tutorial is for Adafruit Motorshield v2 only!
+// Will not work with v1 shields
+
+/*
+ * Have one MultiStepper object with all attached stepper motors
+ * connected to it. This will require you to save what position the stepper motors were in
+ * in the MultiStepper array and tie it to the the shield and stepper number so you can  
+ * access it.
+ */
+
 #include <Wire.h>
- 
- 
-void setup()
-{
-  Wire.begin();
- 
-  Serial.begin(9600);
-  while (!Serial);             // Leonardo: wait for serial monitor
-  Serial.println("\nI2C Scanner");
+#include <AccelStepper.h>
+#include <Adafruit_MotorShield.h>
+#include <MultiStepper.h>
+
+class Steppers{
+  public:
+    void addStepper(uint16_t steps_per_rev, uint8_t stepperNumb, uint8_t shield);
+    Adafruit_StepperMotor getSavedStepper(uint8_t shield, uint8_t stepperNumb);
+    Adafruit_StepperMotor getSavedStepper(int index);
+  private:
+  //Adafruit_MotorShield addresses are uint8_t
+  //stepperNumb is uint8_T
+    uint8_t steppers [10][2];  
+};
+
+void Steppers::addStepper(uint16 steps_per_rev, uint8_t stepperNumb, uint8_t shield){
+  
 }
- 
- 
+
+Adafruit_MotorShield AFMSbot(0x61); // Rightmost jumper closed
+Adafruit_MotorShield AFMStop(0x60); // Default address, no jumpers
+
+// Connect two steppers with 200 steps per revolution (1.8 degree)
+// to the top shield
+Adafruit_StepperMotor *myStepper1 = AFMStop.getStepper(200, 1);
+Adafruit_StepperMotor *myStepper2 = AFMStop.getStepper(200, 2);
+
+// you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
+// wrappers for the first motor!
+void forwardstep1() {  
+  myStepper1->onestep(FORWARD, DOUBLE);
+  //DOUBLE is the high torque
+}
+void backwardstep1() {  
+  myStepper1->onestep(BACKWARD, DOUBLE);
+}
+// wrappers for the second motor!
+void forwardstep2() {  
+  myStepper2->onestep(FORWARD, DOUBLE);
+}
+void backwardstep2() {  
+  myStepper2->onestep(BACKWARD, DOUBLE);
+}
+
+// Now we'll wrap the 3 steppers in an AccelStepper object
+AccelStepper stepper1(forwardstep1, backwardstep1);
+AccelStepper stepper2(forwardstep2, backwardstep2);
+
+MultiStepper steppers;
+
+long positions [2];
+
+void setup()
+{  
+  Serial.begin(9600);
+  AFMSbot.begin(); // Start the bottom shield
+  AFMStop.begin(); // Start the top shield
+   
+  stepper1.setMaxSpeed(100.0);
+  stepper1.setSpeed(100.0);
+  //stepper1.setAcceleration(100.0);
+  //stepper1.moveTo(24);
+  steppers.addStepper(stepper1);
+  
+  stepper2.setMaxSpeed(200.0);
+  stepper2.setSpeed(100.0);
+  //stepper2.setAcceleration(100.0);
+  //stepper2.moveTo(50000);
+  steppers.addStepper(stepper2);
+  positions[0] = 1000;
+  positions[1] = 500;
+}
+
 void loop()
 {
-  byte error, address;
-  int nDevices;
- 
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
- 
-  delay(5000);           // wait 5 seconds for next scan
+  positions[0] += 100;
+  positions[1] += 50;
+  steppers.moveTo(positions);
+  steppers.runSpeedToPosition();
+
+  delay(1000);
+  Serial.println("ho");
 }
