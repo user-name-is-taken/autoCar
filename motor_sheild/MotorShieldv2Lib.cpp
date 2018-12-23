bc84c35ba6598916936e0d5d276270b0087011f6
Michael Lundquist <michaellundquist7@gmail.com>
2018-12-23 12:24:08 -0500

    Done adding getters and setters for the custom stepper motorsclass 'Steppers'. I need to test this class, then I need to add parsers so I can send commands to these motors from the serial monitor.

--- motor_sheild/MotorShieldv2Lib.cpp
+++ motor_sheild/MotorShieldv2Lib.cpp
@@ -175,21 +175,39 @@ class MyAccelStepper: public AccelStepper
  * Each MultiStepper can contain up to 10 objects. This class wraps MultiStepper to
  * handle these objects
  */
-class Steppers{
+class Steppers: public MultiStepper{
   public:
     Steppers(){
       curStepperIndex = 0;
+      //steppersIndexes [10][2] = new uint8_t [10][2];
     }
-    void addStepper(uint16_t steps_per_rev, uint8_t stepperNumb, uint8_t shield){
-        //This function will require writing a function
-  
-      Adafruit_MotorShield AFMS = shields[shield];//parsed out by getMotorShield?
-      static Adafruit_StepperMotor *myStep = AFMS.getStepper(steps_per_rev, stepperNumb);
-      
-      //https://arduino.stackexchange.com/questions/33789/content-is-not-captured
-      //this might not compile?
-      MyAccelStepper curStepper(myStep);
-      steppers.addStepper(curStepper);
+    
+    /**
+     * Adds a stepper motor to act in unison with the other bound stepper motors 
+     * 
+     * params:
+     * stepperNum: the number
+     * with 200 steps
+     */
+    void addStepper(uint8_t stepperNumb, uint8_t shield, uint16_t steps_per_rev = 200){
+      if(curStepperIndex >= 9){
+        //error, too many shields
+      }else if(stepperNumb != 0 and stepperNumb!=1){
+         //Invalid Stepper motor
+      }else if(!shieldConnected(shield)){
+         // shield not connected.
+      }else if(getSavedStepperIndex(shield, stepperNumb) != -1){
+        //error, shield already added
+      }else{  
+        Adafruit_MotorShield AFMS = *shields[shield];//parsed out by getMotorShield?
+        static Adafruit_StepperMotor *myStep = AFMS.getStepper(steps_per_rev, stepperNumb);
+        
+        MyAccelStepper curStepper(myStep);// create a MyAccelStepper named curStepper
+        MultiStepper::addStepper(curStepper);//super class's method
+        curStepperIndex +=1;
+        steppersIndexes[curStepperIndex][0] = shield;
+        steppersIndexes[curStepperIndex][1] = stepperNumb;
+      }
     }
 
     /**
@@ -198,7 +216,7 @@ class Steppers{
      * 
      * Returns: the index in the array if the motor is in the array, otherwise it returns -1
     */
-    unsigned char getSavedStepperIndex(uint8_t shield, uint8_t stepperNumb){
+    uint8_t getSavedStepperIndex(uint8_t shield, uint8_t stepperNumb){
       for(int index = 0; index <= curStepperIndex; index++){
         if(steppersIndexes[index][0] == shield 
           && steppersIndexes[index][1] == stepperNumb){
@@ -207,17 +225,108 @@ class Steppers{
       }
       return -1;
     }
-  protected:
-    MultiStepper steppers;
+
+    /**
+     * Tells the motor to move moveAmount ticks relative to the position it's in.
+     */
+    void setToMove(uint8_t shield, uint8_t stepperNumb, long moveAmount){
+       uint8_t index = getSavedStepperIndex(shield, stepperNumb);
+       long curPos = getPosition(index);
+       curPos += moveAmount;// returned by reference?
+       //setPosition(index, curPos); //not necessary if returned by ref
+    }
+
+    /**
+     * Tells the motor to move moveAmount ticks relative to the position it's in.
+     */
+    void setToMove(uint8_t index, long moveAmount){
+       long curPos = getPosition(index);
+       curPos += moveAmount;// returned by reference?
+       //setPosition(index, curPos); //not necessary if returned by ref
+    }
+
+    /**
+     * Calls moveTo with the stored possitions
+     */
+    void moveTo(){
+      long * posArr = getPosArr();
+      MultiStepper::moveTo(posArr);
+      delete[] posArr;
+      //delete[]: http://www.cplusplus.com/reference/new/operator%20new[]/
+    }
+
+    /**
+     * Gets the positon array after finding the motor. Creates the motor if it doesn't
+     * exist.
+     */
+    long getPosition(uint8_t shield, uint8_t stepperNumb){
+      uint8_t index = getSavedStepperIndex(shield, stepperNumb);
+      if(index > -1){
+         return getPosition(index);
+      }else{
+        //Motor doesn't exist.
+      }
+    }
+    
+    /**
+     * gets the possition array that will be passed to moveTo after memcpy 
+     */
+     long getPosition(uint8_t index){
+       if(index < 10){
+          return positions[index];//return a copy of this instead of the real thing.
+       }else{
+          //error, indexOutOfBounds
+       }
+     }
+
+    /**
+     * Sets the positon array after finding the motor. Creates the motor if it doesn't
+     * exist.
+     */
+    void setPosition(uint8_t shield, uint8_t stepperNumb, long pos){
+      uint8_t index = getSavedStepperIndex(shield, stepperNumb);
+      if(index > -1){
+         setPosition(index, pos);
+      }else{
+        //Motor doesn't exist. Creating the stepperMotor, then set its pos.
+        index = curStepperIndex;
+        addStepper(stepperNumb, shield);
+        setPosition(index, pos);
+      }
+    }
+    
+    /**
+     * sets the possition array that will be passed to moveTo after memcpy 
+     */
+     void setPosition(uint8_t index, long pos){
+       if(index < 10){
+          positions[index] = pos;        
+       }else{
+          //error, indexOutOfBounds
+       }
+     }
+
+     
   private:
   //Adafruit_MotorShield addresses are uint8_t
   //stepperNumb is uint8_T
     uint8_t steppersIndexes [10][2];//[[shield, stepper], [shield, stepper],...]
     unsigned char curStepperIndex;
+    long positions [10];//you pass this to moveTo. Before you do, you have to resize it with memcpy
+    
+    /**
+     * This will add a long[] array to the free store (heap). You MUST delete it with _____.
+     * The array this returns will be passed to moveTo
+     */
+    long * getPosArr(){
+      long * posArr = new long [curStepperIndex + 1];// need to put on "free store"
+      //memcpy: https://stackoverflow.com/questions/19439715/using-memcpy-in-c
+      // https://stackoverflow.com/questions/4643713/c-returning-reference-to-local-variable
+      //new: http://www.cplusplus.com/reference/new/operator%20new[]/
+      memcpy(posArr, steppersIndexes, sizeof *positions);
+      return posArr;
+    }
 };
-
-
-
 
 
 
