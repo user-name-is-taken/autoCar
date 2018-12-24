0bcfb59d585f79e53e091f417d7b5fa94daf86d6
Michael Lundquist <michaellundquist7@gmail.com>
2018-12-24 16:31:16 -0500

    The code wasn't executing because it was too large for the arduino. To fix this I had to only use DC motors or stepper motors.

--- motor_sheild/motor_sheild.ino
+++ motor_sheild/motor_sheild.ino
@@ -10,7 +10,7 @@ Make sure your line separator is set to newline, not carrage return
 */
 
 #include <Wire.h>
-#include "MSv2Motors.h"
+//#include "MSv2Motors.h"
 #include "MSv2Steppers.h"
 // consider including #include <Regexp.h> here because it might be used in multiple libraries.
 // #include <Adafruit_MotorShield.h>
@@ -28,49 +28,58 @@ void setup() {
   usb = new char[50];
   Wire.begin();// super important for my motor shield library
   //char letter;
+
+   //Serial.onReceive(MyFunction);
 }
 
 void loop() {
-  if(Serial.available() > 0){
-    //************read from serial**************
-   while(Serial.available() > 0){
-      char letter = Serial.read();
-      if(letter == '\n')
-        break;//handles the last '\n' char of arduino serial input
-      usb[counter] = letter;
-      counter ++;
-      //the last character is a new line, destroying my regex.
-      //how can I handle this?
-      if(Serial.available() == 0){
-        delay(10);
-        //http://forum.arduino.cc/index.php?topic=22735.0
-        //this waits for the next character if is hasn't yet arrived.
-      }
-   }
-   usb[counter] = '\0';//terminates the string (replacing the newline)
-
-   
-   //***********do stuff with the serial input****************
-   if(checkMSv2Motors(usb, &toWrite)){
-     Serial.println(usb);
-     //https://stackoverflow.com/questions/2229498/passing-by-reference-in-c
-     Serial.println(toWrite);//passing the pointer
-     //NAME + "_" + I wanted to add this, but they're different types
-   }else if(checkMSv2Steppers(usb, &toWrite)){
-     Serial.println(usb);
-     Serial.println(toWrite);//passing the pointer
-   }else if( strcmp(usb,"APIs") == 0 ){//tells what APIs are connected
-     Serial.println( "APIs_" + NAME + "_" + toWrite);
-     //you can check something else here (another API's check...)
-   }
-   counter = 0;
-   /*
-    * note, this program assumes the entire message is in the buffer
-    * when Serial.avaliable reads it, and that another message doesn't
-    * get put into the buffer until after you've read this one.
-    */
- } 
+  MyFunction();
 }
 
+void MyFunction(){
+   if(Serial.available() > 0){
+    Serial.println("in callback");
+    //************read from serial**************
+     while(Serial.available() > 0){
+        char letter = Serial.read();
+        if(letter == '\n')
+          break;//handles the last '\n' char of arduino serial input
+        usb[counter] = letter;
+        counter ++;
+        //the last character is a new line, destroying my regex.
+        //how can I handle this?
+        if(Serial.available() == 0){
+          delay(10);
+          //http://forum.arduino.cc/index.php?topic=22735.0
+          //this waits for the next character if is hasn't yet arrived.
+        }
+     }
+     usb[counter] = '\0';//terminates the string (replacing the newline)
+  
+     
+     //***********do stuff with the serial input****************
+     /*
+     if(checkMSv2Motors(usb, &toWrite)){
+       Serial.println(usb);
+       //https://stackoverflow.com/questions/2229498/passing-by-reference-in-c
+       Serial.println(toWrite);//passing the pointer
+       //NAME + "_" + I wanted to add this, but they're different types
+     }else 
+     */
+     if(checkMSv2Steppers(usb, &toWrite)){
+       Serial.println(usb);
+       Serial.println(toWrite);//passing the pointer
+     }else if( strcmp(usb,"APIs") == 0 ){//tells what APIs are connected
+       Serial.println( "APIs_" + NAME + "_" + toWrite);
+       //you can check something else here (another API's check...)
+     }
+     counter = 0;
+     /*
+      * note, this program assumes the entire message is in the buffer
+      * when Serial.avaliable reads it, and that another message doesn't
+      * get put into the buffer until after you've read this one.
+      */
+   } 
+}
 //TODO: Add a callback when Serial data is received
 //https://playground.arduino.cc/Serial/AttachReceiveFunction
