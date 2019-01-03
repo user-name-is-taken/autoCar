f1d1595230dc95e79006bee6811cc7e375e5709f
michael lundquist <michaellundquist7@gmail.com>
2019-01-03 17:02:26 -0500

    Adding most of the functionality of this file. This essentially represents the arduino itself. The c++ file's NAME should be changed to MSv2 so that when it responds to APIs, it says MSv2.

--- things/src/main/java/com/example/non_admin/picar/MSv2.java
+++ things/src/main/java/com/example/non_admin/picar/MSv2.java
@@ -8,9 +8,12 @@ package com.example.non_admin.picar;
 public class MSv2 extends ArduinoAPI {
     private Shield[] allShields;
 
+    private MSv2Steppers[][] stepperGroups;//see executeGroup for why this is here.
+
     public MSv2 (Device dev){
         super(dev);
         allShields = new Shield[32];
+        stepperGroups = new MSv2Steppers[16][10];
     }
 
     @Override
@@ -18,5 +21,43 @@ public class MSv2 extends ArduinoAPI {
         //log the output.
         //Log errors in an array?
         return false;
+    }
+
+    /**
+     * This sets up a shield at the specified index
+     * These indexes correspond to the I2C addresses.
+     *
+     * @param shieldIndex this is the shield index (0-31), not the I2C address
+     */
+    public void setShield(int shieldIndex){
+        if(getShield(shieldIndex) == null) {
+            this.allShields[shieldIndex] = new Shield(this.dev, shieldIndex);
+        }else{
+            //error, shield already exists
+        }
+    }
+
+    /**
+     * Gets a shield at this index.
+     *
+     * @param shieldIndex the index in allShields to get a Shield from
+     * @return the Shield at this given index
+     */
+    public Shield getShield(int shieldIndex){
+        return this.allShields[shieldIndex];
+    }
+
+    /**
+     * While groups are only relevant to stepper motors, those stepper motors can be
+     * on different shields, so I'm adding this execute group parameter here because it can
+     * act across all shields on an Arduino.
+     * @param group
+     */
+    public void executeGroup(int group){
+        for(MSv2Steppers stepperMotor : stepperGroups[group]){
+            stepperMotor.executeGroup(group);
+        }
+        this.dev.send(String.format(MSv2Steppers.groupExeStr,
+                "0" + Integer.toHexString(group)));
     }
 }
