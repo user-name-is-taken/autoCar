bffe85992415d9d5a497c2b2c02d11a5c1c7e512
michael lundquist <michaellundquist7@gmail.com>
2019-01-10 17:37:42 -0500

    copied code from the [second video](https://www.youtube.com/watch?v=QVD_8PrvG4Q&list=PLgCYzUzKIBE8KHMzpp6JITZ2JxTgWqDH2&index=2). Haven't tested any of this yet.

--- things/src/main/java/com/example/non_admin/picar/MyBluetooth.java
+++ things/src/main/java/com/example/non_admin/picar/MyBluetooth.java
@@ -20,6 +20,58 @@ import static android.content.ContentValues.TAG;
 public class MyBluetooth extends MikeProvider{
     private static Context context;
     private static BluetoothAdapter mBluetoothAdapter;
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    // todo: make this non static? remember, it's unregistered in MainActivity.onDestroy
+    public final BroadcastReceiver btStateBroadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
+                switch (state){
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "onReceive: STATE OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "STATE TURNING OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "STATE ON");
+                        makeDiscoverable(); //todo handle this better?
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "STATE TURNING ON");
+                        break;
+                }
+            }
+        }
+    };
+
+    public final BroadcastReceiver btDiscoverBroadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(mBluetoothAdapter.ACTION_SCAN_MODE_CHANGED)) {
+                int mode = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE,
+                        BluetoothAdapter.ERROR);
+                switch (mode){
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
+                        Log.d(TAG, "Discoverability enabled.");
+                        break;
+                    case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
+                        Log.d(TAG, "Discoverability Disabled. Able to receive connections");
+                        break;
+                    case BluetoothAdapter.SCAN_MODE_NONE:
+                        Log.d(TAG, "Discoverability disabled. Unable to receive connections");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTING:
+                        Log.d(TAG, "connecting...");
+                        break;
+                    case BluetoothAdapter.STATE_CONNECTED:
+                        Log.d(TAG, "Connected.");
+                        break;
+                }
+            }
+        }
+    };
 
 
 
@@ -73,30 +125,21 @@ public class MyBluetooth extends MikeProvider{
         }
     }
 
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    // todo: make this non static? remember, it's unregistered in MainActivity.onDestroy
-    public final BroadcastReceiver btStateBroadcastReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
-                switch (state){
-                    case BluetoothAdapter.STATE_OFF:
-                        Log.d(TAG, "onReceive: STATE OFF");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_OFF:
-                        Log.d(TAG, "STATE TURNING OFF");
-                        break;
-                    case BluetoothAdapter.STATE_ON:
-                        Log.d(TAG, "STATE ON");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_ON:
-                        Log.d(TAG, "STATE TURNING ON");
-                        break;
-                }
-            }
-        }
-    };
+
+    /**
+     * Makes the bluetooth discoverable
+     *
+     * @see this.btDiscoverBroadcastReceiver
+     */
+    private void makeDiscoverable(){
+        int seconds = 300;
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        context.startActivity(discoverableIntent);
+
+        IntentFilter intentFilter = new IntentFilter(mBluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+        context.registerReceiver(this.btDiscoverBroadcastReceiver, intentFilter);
+    }
 
 
 }
