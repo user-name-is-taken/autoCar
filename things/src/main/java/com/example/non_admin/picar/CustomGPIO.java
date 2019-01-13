4cc5c3b97e20d65fc9647b556775560076ff48d2
Michael Lundquist <michaellundquist7@gmail.com>
2019-01-13 16:03:19 -0500

    Adding a GPIO class that listens for binary GPIO input on its corresponding BCM pin, (default is GPIO4, aka BCM4)

--- things/src/main/java/com/example/non_admin/picar/CustomGPIO.java
+++ things/src/main/java/com/example/non_admin/picar/CustomGPIO.java
@@ -1,4 +1,84 @@
 package com.example.non_admin.picar;
 
-public class CustomGPIO {
+import android.content.Context;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
+public class CustomGPIO{
+    private Gpio mGpio;
+    private static final String GPIO_NAME = "BCM4";
+    private GpioCallback mGpioCallback = new GpioCallback() {
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+            try {
+                if (gpio.getValue()) {
+                    //Pin is HIGH
+                    Log.i(TAG, "pin is high");
+                    if(MainActivity.ttsEngine != null && MainActivity.ttsEngine.isAvailable()) {
+                        MainActivity.ttsEngine.speak("pin is high");
+                    }
+                } else {
+                    //pin is LOW
+                    Log.i(TAG, "pin is low");
+                    if(MainActivity.ttsEngine != null && MainActivity.ttsEngine.isAvailable()) {
+                        MainActivity.ttsEngine.speak("pin is low");
+                    }
+                }
+                return true;
+            }catch (IOException e){
+                Log.e(TAG, "gpio.getValue IOexception", e);
+                return false;
+            }
+        }
+
+        @Override
+        public void onGpioError(Gpio gpio, int error) {
+            Log.w(TAG, gpio + ": Error event " + error);
+        }
+
+    };
+
+    public CustomGPIO(){
+        try {
+            PeripheralManager manager = PeripheralManager.getInstance();
+            mGpio = manager.openGpio(GPIO_NAME);
+            mGpio.setDirection(Gpio.DIRECTION_IN);
+            mGpio.setActiveType(Gpio.ACTIVE_HIGH);//between BCM4 and Gnd
+            mGpio.setEdgeTriggerType(Gpio.EDGE_RISING);
+
+        } catch (IOException e) {
+            Log.w(TAG, "Unable to access GPIO", e);
+        }
+
+
+    }
+
+    public void start(){
+        try {
+            mGpio.registerGpioCallback(mGpioCallback);
+        } catch (IOException e) {
+            Log.w(TAG, "Can't link GPIO", e);
+        }
+    }
+
+    public void stop(){
+        mGpio.unregisterGpioCallback(this.mGpioCallback);
+    }
+    public void kill(){
+        try {
+            mGpio.close();
+            mGpio = null;
+        } catch (IOException e) {
+            Log.w(TAG, "Unable to close GPIO", e);
+        }
+
+    }
+
 }
